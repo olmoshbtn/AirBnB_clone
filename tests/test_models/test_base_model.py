468aec7747bd441890f7348dceb57e59ac95fdfb
nicolasportela <2103@holbertonschool.com>
2021-02-17 16:55:23 +0000

    base model test fix

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,25 +14,25 @@ class TestBaseModel(unittest.TestCase):
 
         inst_1 = BaseModel()
         inst_2 = BaseModel()
-        #test if id is string
+        # test if id is string
         self.assertIsInstance(inst_1.id, str)
-        #test not equal id
+        # test not equal id
         self.assertNotEqual(inst_1.id, inst_2.id)
-        #test if created_at is datetime
+        # test if created_at is datetime
         self.assertIsInstance(inst_1.created_at, datetime)
-        #test if update_at is datetime
+        # test if update_at is datetime
         self.assertIsInstance(inst_1.updated_at, datetime)
 
     def test_save(self):
         """Save method testing"""
 
         inst_1 = BaseModel()
-        #test updated_at before and after save()
+        # test updated_at before and after save()
         first_updated_at = inst_1.updated_at
         inst_1.save()
         second_updated_at = inst_1.updated_at
         self.assertNotEqual(first_updated_at, second_updated_at)
-        #test the type of updated_at after save()
+        # test the type of updated_at after save()
         self.assertIsInstance(inst_1.updated_at, datetime)
 
     def test_to_dict(self):
@@ -40,9 +40,9 @@ class TestBaseModel(unittest.TestCase):
 
         inst_1 = BaseModel()
         dict_1 = inst_1.to_dict()
-        #Test dict type
+        # Test dict type
         self.assertIsInstance(dict_1, dict)
-        #test if dictionary updates automatically
+        # test if dictionary updates automatically
         inst_1.save()
         dict_2 = inst_1.to_dict()
         self.assertNotEqual(dict_1["updated_at"], dict_2["updated_at"])

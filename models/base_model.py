815135d32a09c4b694adaecc5357c465b03b68b3
nicolasportela <2103@holbertonschool.com>
2021-02-17 16:28:25 +0000

    base_model fixed

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""Provides base class 'BaseModel' from which all other models will be created
-"""
-import models
+"""This module provides 'BaseModel' as base class
+from which all other models will be created"""
+
 from datetime import datetime
 from uuid import uuid4
 
 
 class BaseModel:
-    """Defines functionality common to all models (attributes/methods)
-    """
+    """Common definition for all models (attributes/methods)"""
+
     def __init__(self):
+        """Instantiation method which defines attributes"""
+
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """String representation of the BaseModel class
-        """
+        """String representation of the BaseModel class"""
+
         return "[{model}] ({ident}) {attrs}".format(
             model=self.__class__.__name__,
             ident=self.id,
@@ -24,13 +26,14 @@ class BaseModel:
             )
 
     def save(self):
-        """pdate updated_at with the current datetime
-        """
+        """This method saves the instance and updates the updated_at time"""
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of the instance
-        """
+        """This method returns a dictionary containing
+        all keys/values of __dict__ of the instance"""
+
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
         dictionary["updated_at"] = self.updated_at.isoformat()

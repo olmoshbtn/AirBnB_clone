b365793322bcb5736057d341ae90df093e022209
nicolasportela <2103@holbertonschool.com>
2021-02-18 03:23:47 +0000

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python3
-""""This module contains 'FilesStorage',
+""""This module contains 'FileStorage',
 the class which serializes/deserializes to/from JSON"""
 
-import models
 import json
+import models
 
 
 class FileStorage:
-	"""This class serializes instances to a JSON file
-	and deserializes a JSON file to instances"""
+    """This class serializes instances to a JSON file
+    and deserializes a JSON file to instances"""
 
-Private class attributes
-__file_path = string con el path al archivo json
-__objects = dictionary con objetos de formato [classname.id]
+    # Private class attributes
+    __file_path = "file.json"  # string which is path to the JSON file
+    __objects = {}  # empty dictionary to store all objects by <class.name>.id
 
-Public instance methods
-all(self) = retorna __objects
-new(self, obj) = setea en __objects el obj [classname.id]
-save(self) = serialize __objects al archivo json
-reload(self) = deserializa el archivo json a __objects (solo si el path de jason existe)
+    # Public instance methods
+    def all(self):
+        """returns the dictionary __objects"""
 
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+
+        new__objects = {}
+        for key in self.__objects:
+            new__objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, "w", encoding="UTF8") as file:
+            file.write(json.dumps(new__objects))
+
+    def reload(self):
+        """deserializes JSON file to __objects"""
+
+        try:
+            with open(self.__file_path, "r") as file:
+                __objects = json.load(file)
+            for key, value in __objects.items():
+                self.__objects[key] = eval(value["__class__"])(**value)
+        except:
+            pass
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,3 +1,1 @@
 #!/usr/bin/python3
-"""This module contains 'FilesStorage',
-the class which serializes/deserializes to/from JSON"""

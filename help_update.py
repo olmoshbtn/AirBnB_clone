b0e2c5ef82fab800acddc4149bb3c75eebfc5342
olmoshbtn <2120@holbertonschool.com>
2021-02-19 02:25:32 +0000

    help_update.py

---
+++ help_update.py
@@ -0,0 +1,21 @@
+    def help_quit(self):
+        print("Quit command to exit the program")
+
+    def help_EOF(self):
+        print("CTRL + D (EOF) to exit the program")
+
+    def help_create(self):
+        print("Usage: create <valid class name>")
+
+    def help_show(self):
+        print("Usage: show <valid class name> <valid id>")
+
+    def help_destroy(self):
+        print("Usage: destroy <valid class name> <valid id>")
+
+    def help_all(self):
+        print("Usage: all OR all <valid class name>")
+
+    def help_update(self):
+        print("Usage: update <valid class name>", end="")
+        print("<valid id> <attribute name> <attribute value>")
